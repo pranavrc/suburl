e25336eaed72a67a4576d54fbf87a551445a4e4b
Pranav Ravichandran <me@onloop.net>
2013-05-20 18:22:40 +0530

    Simple db model and store object.

---
+++ renderer.lisp
@@ -0,0 +1,21 @@
+(asdf:operate 'asdf:load-op :cl-who)
+(asdf:operate 'asdf:load-op :restas)
+
+(restas:define-module :restas.renderer
+    (:use :cl))
+
+(in-package :restas.renderer)
+(restas:debug-mode-on)
+
+(setf *store* (open-store '(:clsql (:sqlite3 "/tmp/suburl.db"))))
+
+(defpclass urlModel ()
+  ((longUrl :reader longUrl
+	    :initarg :longUrl
+	    :index t)
+   (shortUrl :accessor shortUrl
+	     :initarg :shortUrl
+	     :index t)))
+
+(defun getUrl (inputUrl)
+  (get-instance-by-value 'urlModel 'shortUrl inputUrl))
