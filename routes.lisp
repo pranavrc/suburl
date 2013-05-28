d68c0820d7eaffe81084e209b923fe94cbed9ad3
Pranav Ravichandran <me@onloop.net>
2013-05-28 23:36:52 +0530

    Fixing up responses to different user events.

--- routes.lisp
+++ routes.lisp
@@ -10,14 +10,29 @@
 (in-package :restas.routes)
 (restas:debug-mode-on)
 
+(defparameter *response* "")
+
 (setf (who:html-mode) :html5)
 
 (restas:define-route main ("" :method :get)
   (pathname "~/workbase/suburl/res/index.html"))
 
 (restas:define-route urlSubmit ("" :method :post)
-  (storage::addPair (hunchentoot:post-parameter "longURL") (hunchentoot:post-parameter "shortURL"))
-  (who:with-html-output-to-string (out)
-    (:a :href (hunchentoot:post-parameter "longURL") (who:str (hunchentoot:post-parameter "shortURL")))))
+  (cond
+    ((storage::longUrlExists (hunchentoot:post-parameter "longURL"))
+     (setf *response* 
+	   (who:with-html-output-to-string (out)
+	     (:p "Link already exists at Short URL: ")
+	     (:a :href 
+		 (hunchentoot:post-parameter "longURL")
+		 (who:str
+		  (storage::shortUrl (storage::getlongUrl (hunchentoot:post-parameter "longURL"))))))))
+    ((storage::shortUrlExists (hunchentoot:post-parameter "shortURL"))
+     (setf *response* "Short URL exists. Try another."))
+    (t (progn
+	 (storage::addPair (hunchentoot:post-parameter "longURL") (hunchentoot:post-parameter "shortURL"))
+	 (setf *response* (who:with-html-output-to-string (out)
+			    (:a :href (hunchentoot:post-parameter "longURL") (who:str (hunchentoot:post-parameter "shortURL"))))))))
+  *response*)
 
 (restas:start :restas.routes :port 8080)
