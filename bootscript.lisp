0083ddeaf880b8b0b94988ef8e766d68da12907e
Pranav Ravichandran <me@onloop.net>
2013-06-10 05:07:27 +0400

    Push to asdf registry before loading.

--- bootscript.lisp
+++ bootscript.lisp
@@ -1,6 +1,7 @@
 (require 'asdf)
 
 (asdf:operate 'asdf:load-op '#:restas)
+(push "/home/vanharp/workbase/suburl/" asdf:*central-registry*)
 (asdf:operate 'asdf:load-op '#:suburl)
 ;;(restas:start '#:restas.routes :port 8081)
 (load "/home/vanharp/workbase/suburl/routes.lisp")
